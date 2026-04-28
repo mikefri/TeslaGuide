05551c82b9ea78c8e0d59cdedb39bb56d521f611
Micfri <44413144+mikefri@users.noreply.github.com>
2026-04-28 18:05:40 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'teslaguide-v1';
+const PRECACHE = [
+  '/TeslaGuide/',
+  '/TeslaGuide/blog/',
+  '/TeslaGuide/assets/css/main.css',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
